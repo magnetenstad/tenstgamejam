d25ae769e691caa995106fcc823c345c4433dacd
magnetenstad <tenstad.magne@gmail.com>
2021-04-05 14:39:08 +0200

    Separate Vehicle and Player

--- Vehicle.gd
+++ Vehicle.gd
@@ -1,16 +1,10 @@
 extends VehicleBody
 
-func _ready():
-	pass
-
+class_name Vehicle
 
 func _process(delta):
+	_vehicle_process(delta)
+	
+func _vehicle_process(delta):
+	set_steering(get_steering() * (1 - abs(get_engine_force()) / 10000))
 	set_engine_force(0)
-	if Input.is_action_pressed("ui_up"):
-		set_engine_force(200)
-	if Input.is_action_pressed("ui_down"):
-		set_engine_force(-200)
-	if Input.is_action_pressed("ui_left"):
-		set_steering(get_steering() - 0.01)
-	if Input.is_action_pressed("ui_right"):
-		set_steering(get_steering() + 0.01)
