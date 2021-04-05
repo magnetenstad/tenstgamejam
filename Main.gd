7901509441461493dd027669ec8e5b1d7d6c0423
magnetenstad <tenstad.magne@gmail.com>
2021-04-05 16:10:32 +0200

    Add Map

--- Main.gd
+++ Main.gd
@@ -1,12 +1,7 @@
 extends Spatial
 
-onready var mesh = load("res://assets/Street_0.obj")
-onready var mat0 = load("res://assets/materials/asphalt.tres")
-onready var mat1 = load("res://assets/materials/curb.tres")
-
-var tile
+var map
 
 func _ready():
-	tile = Tile.new(mesh, [mat0, mat1])
-	add_child(tile)
-	
+	map = Map.new()
+	add_child(map)
--- Map.gd
+++ Map.gd
@@ -1,3 +1,14 @@
 extends Node
 
-func createTile
+class_name Map
+
+onready var mesh = load("res://assets/Street_0.obj")
+onready var mat0 = load("res://assets/materials/asphalt.tres")
+onready var mat1 = load("res://assets/materials/curb.tres")
+
+func _ready():
+	for x in range(-4, 4):
+		for z in range(-4, 4):
+			var tile = Tile.new(mesh, [mat0, mat1])
+			tile.transform = tile.transform.translated(Vector3(x*20, 0, z*20))
+			add_child(tile)
