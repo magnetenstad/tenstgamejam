2809452356ae42f91abf3964ebc53959ed98de4b
magnetenstad <tenstad.magne@gmail.com>
2021-04-05 21:02:58 +0200

    Deprecate VehicleBody

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-onready var player = $Player
+#onready var player = $Player
 var camera
 var map
 
@@ -9,6 +9,6 @@ func _ready():
 	map.scale.x = 3
 	map.scale.z = 3
 	add_child(map)
-	camera = VehicleCamera.new(player)
-	camera.set_offset(Vector3(0, 10, -10))
-	add_child(camera)
+	#camera = VehicleCamera.new(player)
+	#camera.set_offset(Vector3(0, 10, -10))
+	#add_child(camera)
--- Vehicle.gd
+++ Vehicle.gd
@@ -1,10 +1,16 @@
-extends VehicleBody
+extends Node
 
-class_name Vehicle
 
-func _process(delta):
-	_vehicle_process(delta)
-	
-func _vehicle_process(_delta):
-	set_steering(get_steering() * (1 - abs(get_engine_force()) / 10000))
-	set_engine_force(0)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
