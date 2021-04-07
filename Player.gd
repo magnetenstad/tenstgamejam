d8e34e5241dd614af436e5bcf42b49ac47cb2451
magnetenstad <tenstad.magne@gmail.com>
2021-04-08 00:10:45 +0200

    Undeprecate VehicleBody

--- Player.gd
+++ Player.gd
@@ -7,7 +7,7 @@ func _vehicle_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		set_engine_force(-200)
 	if Input.is_action_pressed("ui_left"):
-		set_steering(get_steering() + 0.01)
+		set_steering(min(PI/4, get_steering() + 0.01))
 	if Input.is_action_pressed("ui_right"):
-		set_steering(get_steering() - 0.01)
+		set_steering(max(-PI/4, get_steering() - 0.01))
 	
