5f9151b21cc859a14e88ee5e927deb756e23acc4
TorsteinTenstad <52585723+TorsteinTenstad@users.noreply.github.com>
2021-04-10 19:24:07 +0200

    Change road curve and add trailer

--- Player.gd
+++ Player.gd
@@ -3,11 +3,11 @@ extends Vehicle
 func _vehicle_process(delta):
 	._vehicle_process(delta)
 	if Input.is_action_pressed("ui_up"):
-		set_engine_force(200)
+		set_engine_force(500)
 	if Input.is_action_pressed("ui_down"):
-		set_engine_force(-200)
+		set_engine_force(-500)
 	if Input.is_action_pressed("ui_left"):
-		set_steering(min(PI/4, get_steering() + 0.01))
+		set_steering(min(PI/4, get_steering() + 0.005))
 	if Input.is_action_pressed("ui_right"):
-		set_steering(max(-PI/4, get_steering() - 0.01))
+		set_steering(max(-PI/4, get_steering() - 0.005))
 	
